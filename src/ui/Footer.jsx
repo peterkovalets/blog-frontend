9cc634372b7790443f1e46067d168c6a5f79b263
Peter Kovalets <peterkovalets@gmail.com>
2023-08-22 13:44:19 +0700

    Changed name to ReactifyHub

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-slate-950 py-4 text-slate-50">
       <Container>
-        <p>Blog {new Date().getFullYear()}</p>
+        <p>ReactifyHub {new Date().getFullYear()}</p>
       </Container>
     </footer>
   );
--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 function Logo() {
   return (
-    <Link to="/" className="text-xl font-medium uppercase tracking-wide">
-      Blog
+    <Link to="/" className="text-xl font-medium tracking-wide">
+      ReactifyHub
     </Link>
   );
 }
